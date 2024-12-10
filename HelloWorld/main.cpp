f83c7390fc61b9aef83df7b25e3dc33310a3f51a
MylesColeman <mylescoleman1011@gmail.com>
2024-12-10 13:58:03 +0000

    Week 1 Upload

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	cout << "Hello World" << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int temp;
+//	cout << "Enter your temperature" << endl;
+//	cin >> temp;
+//	float convertedTemp = (temp - 32) * 5.0f / 9.0f;
+//	cout << "Your temperature in celsius is " << convertedTemp << endl;
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	int value1{ 1000000 };
+//	int value2 = 3000000000;
+//	int result = value1 * value2;
+//	cout << "The result is " << result << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	string name;
+//	cout << "Enter your name" << endl;
+//	cin >> name;
+//	cout << "Hello " << name << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	float length;
+//	cout << "Enter the length of the room (in metres)" << endl;
+//	cin >> length;
+//	float depth;
+//	cout << "Enter the depth of the room (in metres)" << endl;
+//	cin >> depth;
+//	float height;
+//	cout << "Enter the height of the room (in metres)" << endl;
+//	cin >> height;
+//
+//	float volume = length * depth * height;
+//
+//	cout << "The volume of the room is " << volume << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	cout << "Here is a menu, choose one item: a + b + c" << endl;
+//	char chosenItem;
+//	cin >> chosenItem;
+//
+//	cout << "You chose " << chosenItem << endl;
+//
+//	return 0;
+//}
+
+int main()
+{
+	int a = 20;
+	cout << "The memory address of a is " << &a << endl;
+
+	return 0;
+}
